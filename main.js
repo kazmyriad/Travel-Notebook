b87c9db9dfc4ab4f5b3f853b33db7c1ad5de78f1
Alyssa F <aiepillios1101@gmail.com>
2025-12-04 14:35:45 -0500

    log CSS

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 // We can worry about the JS towards the end
 
+// Page Navigation
 const openPage2 = () =>
 {
     console.log("BUTTON");
     window.location.href= "./page2.html";
 }
 document.querySelector(".create-new").addEventListener("click", openPage2);
+
+const openPage3 = () =>
+{
+    console.log("BUTTON");
+    window.location.href= "./page3.html";
+}
+document.querySelector(".open-log").addEventListener("click", openPage3);
 
 
 // testing map usage, trying to figure how to make a map appear with APIs
